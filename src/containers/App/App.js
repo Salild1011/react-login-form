c33bd1ad6d348e6422502b0275ed27d0e5c777bf
Salild1011 <salild1011@gmail.com>
2019-12-23 00:34:19 -0500

    Basic navbar

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar bg="primary" variant="dark">
+        <Navbar.Brand href="#home">Login Form Demo</Navbar.Brand>
+        <Nav className="ml-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Login</Nav.Link>
+          <Nav.Link href="#pricing">Signup</Nav.Link>
+        </Nav>
+      </Navbar>
     </div>
   );
 }
